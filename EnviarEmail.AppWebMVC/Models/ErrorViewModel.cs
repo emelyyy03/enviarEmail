44c10c1ae11651892e1fbba0cbee057753a07792
Johana Emely Tobar Cruz <emelyyy03@outlook.com>
2025-04-30 10:30:28 -0600

    Agregar archivos de proyecto.

---
+++ EnviarEmail.AppWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EnviarEmail.AppWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
